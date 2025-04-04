76ca1bec3355371bd14f4d1673738a7e7e924f06
Yunong Wu <yunongwu33@gmail.com>
2025-04-04 11:50:31 -0400

    test file

---
+++ test_file.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -A r01304
+#SBATCH -p general
+#SBATCH --job-name=test_file
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=32G
+
+#SBATCH --output=/N/u/wuyuno/Quartz/Documents/BrainStimulation-paper/output/test_file_%A_%a.out.txt
+#SBATCH --error=/N/u/wuyuno/Quartz/Documents/BrainStimulation-paper/error/test_file_%A_%a.err.txt
+#SBATCH --mail-user=wuyuno@iu.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time=4-00:00:00
+
+module load r
+module load geos
+module load udunits
+module load proj
+module load gdal
+
+R CMD BATCH /N/u/wuyuno/Quartz/Documents/GitHub/BrainStimulation-paper/test_file.R
