9cfe0ed353e38179c871c08fdc61ddcba2b6180f
Mandy Mejia <mandy.mejia@gmail.com>
2025-05-06 11:32:34 -0400

    initial visualizations

---
+++ visualize.R
@@ -0,0 +1,28 @@
+setwd('~/Documents/GitHub/BrainStimulation-paper/')
+
+library(ciftiTools)
+ciftiTools.setOption('wb_path', '/Applications/')
+
+library(BayesfMRI)
+
+fnames_GLM <- list.files('Results')
+
+region <- c('ACC1','ACC2','NAcc1','NAcc2')
+
+for(r in region){
+  
+  print(r)
+  fnames_r <- fnames_GLM[grepl(r, fnames_GLM)]
+  
+  for(f in fnames_r){
+    
+    print(f)
+    GLM_f <- readRDS(file.path('Results', f))  
+    
+    #pfname <- gsub('_.+','', f)
+    fname_plot <- gsub('_BayesGLM_result.rds','',f)
+    plot(GLM_f, zlim = c(-0.1,0.1), fname = file.path('Plots', fname_plot))
+  }
+  
+}
+
